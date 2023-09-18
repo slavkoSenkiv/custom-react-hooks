6501a8a829a8e6986277f408d6533a0182a09ab5
CodeSandbox <support@codesandbox.io>
2023-09-18 14:56:53 +0000

    Sandbox snapshot commit

--- backend/app.js
+++ backend/app.js
@@ -1,179 +1,53 @@
-import fs from "node:fs/promises";
+import fs from 'node:fs/promises';
 
-import bodyParser from "body-parser";
-import express from "express";
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const app = express();
 
+app.use(express.static('backend/images'));
 app.use(bodyParser.json());
-app.use(express.static("backend/public"));
+
+// CORS
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
+app.get('/places', async (req, res) => {
+  const fileContent = await fs.readFile('./backend/data/places.json');
+
+  const placesData = JSON.parse(fileContent);
+
+  res.status(200).json({ places: placesData });
 });
 
-app.get("/events", async (req, res) => {
-  const { max, search } = req.query;
-  const eventsFileContent = await fs.readFile("./backend/data/events.json");
-  let events = JSON.parse(eventsFileContent);
+app.get('/user-places', async (req, res) => {
+  const fileContent = await fs.readFile('./backend/data/user-places.json');
 
-  if (search) {
-    events = events.filter((event) => {
-      const searchableText = `${event.title} ${event.description} ${event.location}`;
-      return searchableText.toLowerCase().includes(search.toLowerCase());
-    });
-  }
+  const places = JSON.parse(fileContent);
 
-  if (max) {
-    events = events.slice(events.length - max, events.length);
-  }
-
-  res.json({
-    events: events.map((event) => ({
-      id: event.id,
-      title: event.title,
-      image: event.image,
-      date: event.date,
-      location: event.location,
-    })),
-  });
+  res.status(200).json({ places });
 });
 
-app.get("/events/images", async (req, res) => {
-  const imagesFileContent = await fs.readFile("./backend/data/images.json");
-  const images = JSON.parse(imagesFileContent);
+app.put('/user-places', async (req, res) => {
+  const places = req.body.places;
 
-  res.json({ images });
+  await fs.writeFile('./backend/data/user-places.json', JSON.stringify(places));
+
+  res.status(200).json({ message: 'User places updated!' });
 });
 
-app.get("/events/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const eventsFileContent = await fs.readFile("./backend/data/events.json");
-  const events = JSON.parse(eventsFileContent);
-
-  const event = events.find((event) => event.id === id);
-
-  if (!event) {
-    return res
-      .status(404)
-      .json({ message: `For the id ${id}, no event could be found.` });
+// 404
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
   }
-
-  setTimeout(() => {
-    res.json({ event });
-  }, 1000);
+  res.status(404).json({ message: '404 - Not Found' });
 });
 
-app.post("/events", async (req, res) => {
-  const { event } = req.body;
-
-  if (!event) {
-    return res.status(400).json({ message: "Event is required" });
-  }
-
-  console.log(event);
-
-  if (
-    !event.title?.trim() ||
-    !event.description?.trim() ||
-    !event.date?.trim() ||
-    !event.time?.trim() ||
-    !event.image?.trim() ||
-    !event.location?.trim()
-  ) {
-    return res.status(400).json({ message: "Invalid data provided." });
-  }
-
-  const eventsFileContent = await fs.readFile("./backend/data/events.json");
-  const events = JSON.parse(eventsFileContent);
-
-  const newEvent = {
-    id: Math.round(Math.random() * 10000).toString(),
-    ...event,
-  };
-
-  events.push(newEvent);
-
-  await fs.writeFile("./backend/data/events.json", JSON.stringify(events));
-
-  res.json({ event: newEvent });
-});
-
-app.put("/events/:id", async (req, res) => {
-  const { id } = req.params;
-  const { event } = req.body;
-
-  if (!event) {
-    return res.status(400).json({ message: "Event is required" });
-  }
-
-  if (
-    !event.title?.trim() ||
-    !event.description?.trim() ||
-    !event.date?.trim() ||
-    !event.time?.trim() ||
-    !event.image?.trim() ||
-    !event.location?.trim()
-  ) {
-    return res.status(400).json({ message: "Invalid data provided." });
-  }
-
-  const eventsFileContent = await fs.readFile("./backend/data/events.json");
-  const events = JSON.parse(eventsFileContent);
-
-  const eventIndex = events.findIndex((event) => event.id === id);
-
-  if (eventIndex === -1) {
-    return res.status(404).json({ message: "Event not found" });
-  }
-
-  events[eventIndex] = {
-    id,
-    ...event,
-  };
-
-  await fs.writeFile("./backend/data/events.json", JSON.stringify(events));
-
-  setTimeout(() => {
-    res.json({ event: events[eventIndex] });
-  }, 1000);
-});
-
-app.delete("/events/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const eventsFileContent = await fs.readFile("./backend/data/events.json");
-  const events = JSON.parse(eventsFileContent);
-
-  const eventIndex = events.findIndex((event) => event.id === id);
-
-  if (eventIndex === -1) {
-    return res.status(404).json({ message: "Event not found" });
-  }
-
-  events.splice(eventIndex, 1);
-
-  await fs.writeFile("./backend/data/events.json", JSON.stringify(events));
-
-  setTimeout(() => {
-    res.json({ message: "Event deleted" });
-  }, 1000);
-});
-
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(3000);
